56c2c554d30d889efde2cb4625c3ea1fb35c84b6
tj11 Johnston <tedwardj11@gmail.com>
2018-04-10 18:04:14 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -26,7 +26,7 @@ function pressIt() {
 
 //submitIt
 function submitIt() {
-  $("form")[1].on('submit', function() {
+  $("form").on('submit', function() {
     window.alert('Your form is going to be submitted now')
     return;
   })
