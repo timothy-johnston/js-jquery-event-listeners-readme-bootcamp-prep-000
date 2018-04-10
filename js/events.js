4cbb1b8b1873b3eb2974d7234682a873273075d2
tj11 Johnston <tedwardj11@gmail.com>
2018-04-10 18:09:02 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,54 @@
 //define functions here
 
+//getIt
+function getIt() {
+  $("p").on("click", function() {
+    window.alert("Hey!")
+  })
+  
+}
+
+//frameIt
+function frameIt() {
+  $("img").on('load', function() {
+    $("img").addClass("tasty")
+  })
+}
+
+//pressIt
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if (key.which == 71) {
+      alert('g was pressed');
+    }
+  })
+}
+
+//submitIt
+function submitIt() {
+  
+  const form = window.$('form')[0];
+  
+  $(form).on('submit', function() {
+    window.alert('Your form is going to be submitted now.')
+  })
+  
+  //$("#typing").on('submit', function() {
+  //  window.alert('Your form is going to be submitted now')
+  //  return;
+  //})
+}
+
 $(document).ready(function(){
 
 // call functions here
+getIt()
+
+frameIt()
+
+pressIt()
+
+submitIt()
+
 
 });
