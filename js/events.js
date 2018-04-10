cebc105575c7d1d33d792b012dc55e7bc2e0f0cb
tj11 Johnston <tedwardj11@gmail.com>
2018-04-10 17:57:41 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,42 @@
 //define functions here
 
+//getIt
+function getIt() {
+  $("p").on("click", function() {
+    window.alert("Hey!")
+  })
+  
+}
+
+//frameIt
+function frameIt() {
+  $("img").on('load', function() {
+    $("img").addClass("tasty")
+  })
+}
+
+//pressIt
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if (key.which == 71) {
+      alert('g was pressed');
+    }
+  })
+}
+
+//submitIt
+function submitIt() {
+  
+}
+
 $(document).ready(function(){
 
 // call functions here
+getIt()
+
+frameIt()
+
+pressIt()
+
 
 });
